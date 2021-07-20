749402f26f9ed28e2b1bdb0a5e587c349e3c2bc9
Jezer <jezerrazuri@upeu.edu.pe>
2021-07-19 23:48:07 -0500

    lasdfas

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,19 @@
 
+const passport = require('passport');
 const { basicAuth } = require('../controllers/auth.controller');
 const authRouter = require('express').Router();
-
+const boom = require('@hapi/boom')
+require('../utils/auth/jwt')
 
 authRouter.post('/token', basicAuth)
+authRouter.post('/validate', passport.authenticate("jwt", { session: false }), (req, res, next) => {
+
+
+    if (!req.user) {
+        return next(boom.unauthorized())
+    }
+
+    return res.status(200).json('Valid Token')
+})
 
 module.exports = authRouter;
